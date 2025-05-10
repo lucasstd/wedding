c54b5228523b5f00e8589d21a383bd30c3720c0d
lucasstd <lucas.staudt@acad.pucrs.br>
2025-05-10 17:19:32 -0300

    add initial commits

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': '#103650',
+      },
+    },
+  },
+  plugins: [],
+}
