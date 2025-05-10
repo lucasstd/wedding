daafe9c9a040ca2040079def6fa1fb8991a4accf
lucasstd <lucas.staudt@acad.pucrs.br>
2025-05-10 18:35:28 -0300

    fix index removing unsuse line

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+  base: '/',
 });
